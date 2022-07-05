1acfd11028c7411b7bb44ec30af57b4f26e10e8e
Carlos.Arroyoh <carlos.arroyoh@uanl.mxm>
2022-07-05 12:09:50 -0500

    Creacion de errorPage

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
