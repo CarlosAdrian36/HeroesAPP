83a49db4f312ab3e3f8942d158eef5ca302bad61
Carlos.Arroyoh <carlos.arroyoh@uanl.mxm>
2022-07-07 09:52:23 -0500

    Creacion de las tarjetas y estilos en el zoom

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,18 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
+import { Heroe } from '../../interface/heroe.interface';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styles: [
+  styles: [ `
+    mat-card{
+      margin-top : 20px
+    }
+  `
   ]
 })
 export class ListadoComponent implements OnInit {
 
+  heroes :Heroe[] = [];
+
   constructor(private HeroesService: HeroesService) { }
 
   ngOnInit(): void {
-    this.HeroesService.getHeroes().subscribe(resp =>console.log(resp));
+    this.HeroesService.getHeroes().subscribe(H => {
+      
+      this.heroes = H;
+
+    });
   }
 
 }
