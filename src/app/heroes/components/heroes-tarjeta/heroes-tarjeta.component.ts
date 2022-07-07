e433dcd25410436fa575a691502e6563f2047236
Carlos.Arroyoh <carlos.arroyoh@uanl.mxm>
2022-07-07 10:14:18 -0500

    Implementacion del componente heroes-tarjeta

--- src/app/heroes/components/heroes-tarjeta/heroes-tarjeta.component.ts
+++ src/app/heroes/components/heroes-tarjeta/heroes-tarjeta.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Heroe } from '../../interface/heroe.interface';
 
 @Component({
   selector: 'app-heroes-tarjeta',
   templateUrl: './heroes-tarjeta.component.html',
   
 })
-export class HeroesTarjetaComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class HeroesTarjetaComponent   {
+  
+  @Input() heroe!: Heroe;
 
 }
