b9ab7dedca29e23710fbc9dd00762437b43e473f
Carlos.Arroyoh <carlos.arroyoh@uanl.mxm>
2022-07-12 12:18:45 -0500

    Creacion de la autenticacion proteccion de ruta y solucion de perdida de datos del usuario

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,19 +1,51 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree, Router } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements  CanLoad {
-  // canActivate(
-  //   route: ActivatedRouteSnapshot,
-  //   state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-  //   return true;
-  // }
+export class AuthGuard implements CanActivate, CanLoad {
+  
+  constructor ( private AuthService: AuthService,
+                private router :Router) {}
+  
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean > | Promise<boolean> | boolean {
+    
+    //   if(this.AuthService.auth.id){
+    //     return true;
+    //   }
+    //   console.log('Bloqueado por el AuthGuard-CanActivate');
+    // return false;
+
+    return this.AuthService.verificaAutenticacion().pipe(tap(
+      estaAutenticado => {
+        if (!estaAutenticado){
+          this.router.navigate(['./auth/login']);
+        }
+      })
+    )
+  }
+
+
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean   {
-    return true;
+
+      return this.AuthService.verificaAutenticacion().pipe(tap(
+        estaAutenticado => {
+          if (!estaAutenticado){
+            this.router.navigate(['./auth/login']);
+          }
+        })
+      )
+    //   if(this.AuthService.auth.id){
+    //     return true;
+    //   }
+    //   console.log('Bloqueado por el AuthGuard -CanLoad');
+    // return false;
   }
 }
